a051604543f56c48f46a31ffe3d43228a9d8046d
userlaiza <laiza.silva@escolar.ifrn.edu.br>
2026-06-15 13:41:02 +0000

    Corrige factory e organiza blueprint de autenticação

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -1,13 +1,5 @@
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-
-    @app.route("/")
-    def hello():
-        return "Hello, World!"
-
-    return app
 
 import os
 from flask import Flask
