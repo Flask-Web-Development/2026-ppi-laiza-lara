611fcf9edd5a2cc3c07791dc923dde57fa4ad3e5
userlaiza <laiza.silva@escolar.ifrn.edu.br>
2026-06-15 13:04:18 +0000

    Adiciona rotas básicas e rotas dinâmicas

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, Flask!"
+
+@app.route("/hello")
+def hello():
+    return "Olá, mundo!"
+
+@app.route("/user/<name>")
+def user(name):
+    return f"Usuário: {name}"
