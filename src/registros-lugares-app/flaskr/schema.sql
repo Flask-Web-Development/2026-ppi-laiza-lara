d92a84ff6f90b8eeaac86abb8912cda034aa8273
userlaiza <laiza.silva@escolar.ifrn.edu.br>
2026-07-13 23:22:00 +0000

    chore: organizar estrutura do projeto (#1)

--- src/registros-lugares-app/flaskr/schema.sql
+++ src/registros-lugares-app/flaskr/schema.sql
@@ -11,8 +11,7 @@ CREATE TABLE post (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  nome do lugar TEXT NOT NULL,
-  dia TEXT NOT NULL,
-  o que achou TEXT NOT NULL,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
