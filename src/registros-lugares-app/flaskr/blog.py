ac6d7f9f95d46ffee7bf57d44fa550c2ac56bf19
userlaiza <laiza.silva@escolar.ifrn.edu.br>
2026-07-14 01:45:00 +0000

    feat: create lugar database table e começando o CRUD(#3)

--- src/registros-lugares-app/flaskr/blog.py
+++ src/registros-lugares-app/flaskr/blog.py
@@ -12,34 +12,35 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+    lugar = db.execute(
+        'SELECT l.id, nome, descricao, data_visita, author_id, username'
+        ' FROM lugar l JOIN user u ON l.author_id = u.id'
+        ' ORDER BY data_visita DESC'
     ).fetchall()
 
-    return render_template('blog/index.html', posts=posts)
+    return render_template('blog/index.html', lugar=lugar)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        data_visita = request.form['data_visita']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not nome:
+            error = 'nome is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO lugar (nome, descricao, data_visita, author_id)'
+                ' VALUES (?, ?, ?, ?)',
+                (nome, descricao, data_visita, g.user['id'])
             )
             db.commit()
 
@@ -48,56 +49,58 @@ def create():
     return render_template('blog/create.html')
 
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
+def get_lugar(id, check_author=True):
+    lugar = get_db().execute(
+        'SELECT l.id, nome, descricao, data_visita, author_id, username'
+        ' FROM lugar l JOIN user u ON l.author_id = u.id'
+        ' WHERE l.id = ?',
         (id,)
     ).fetchone()
 
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
+    if lugar is None:
+        abort(404, f"Lugar id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and lugar['author_id'] != g.user['id']:
         abort(403)
 
-    return post
+    return lugar
 
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    post = get_post(id)
+    lugar = get_lugar(id)
 
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        data_visita = request.form['data_visita']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not nome:
+            error = 'nome is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE lugar SET nome = ?, descricao = ?, data_visita = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (nome, descricao, data_visita, id)
             )
             db.commit()
 
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/update.html', post=post)
+    return render_template('blog/update.html', lugar=lugar)
+
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
-    get_post(id)
+    get_lugar(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM lugar WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
